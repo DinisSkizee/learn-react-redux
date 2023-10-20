010554a259e9b3d46b74c3f73b262b32ff904851
DinisSkizee <dinissilvauk@gmail.com>
2023-10-20 11:58:37 +0100

    add small interface in sass

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useAppDispatch, useAppSelector } from "./store/store";
 import { Todo } from "./store/todos/todosSlice";
 import { useEffect } from "react";
 import { FETCH_TODOS } from "./store/todos/types";
+import "./App.sass";
 
 function App() {
   const todos: Todo[] = useAppSelector((state) => state.todos.todos);
@@ -13,13 +14,24 @@ function App() {
   }, [dispatch]);
 
   return (
-    <>
-      <div>
+    <div className="wrapper">
+      <div className="items-wrapper">
         {todos?.map((cur) => (
-          <div key={cur.id}>{cur.text}</div>
+          <div key={cur.id}>
+            {cur.text}
+            <br />
+            <span>{cur.completed ? "Completed" : "Not Completed"}</span>
+          </div>
         ))}
       </div>
-    </>
+
+      <div className="add-item-wrapper">
+        <div>
+          <input type="text" />
+          <button>Add Item</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
